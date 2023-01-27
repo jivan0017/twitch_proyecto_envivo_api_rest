02cc47d1eed4094141825ed435f487ddfd492024
Andres Segura <andres.segura.dev@gmail.com>
2023-01-27 15:04:29 -0500

    set: opción de consulta apiUrl desde varables de entorno.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 import { createApp } from 'vue'
-// TODO: agregar
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
 
 // TODO: agregar
+import './assets/main.css'
 import "bootstrap/dist/js/bootstrap.min";
+import "bootstrap/dist/css/bootstrap.min.css";
